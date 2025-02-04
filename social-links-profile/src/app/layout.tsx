fe55f20d158c998d2087166b2f23d62959216e39
henriquesbarros <henriquesbarroshs@gmail.com>
2025-02-04 08:53:51 -0300

    first commit

---
+++ social-links-profile/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Social Links Profile",
+  description: "Social Links Profile",
+  icons: {
+    icon: "/favicon-32x32.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
+
+
---
+++ social-links-profile/tailwind.config.ts
@@ -0,0 +1,46 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        grey: {
+          900: "#141414",
+          800: "#1F1F1F",
+          700: "#333333",
+        },
+        green: "#C4F82A",
+        white: "#FFFFFF",
+      },
+      fontSize: {
+        preset1: [
+          "24px",
+          { lineHeight: "150%", letterSpacing: "0px", fontWeight: "700" },
+        ], // Bold
+        preset2: [
+          "14px",
+          { lineHeight: "150%", letterSpacing: "0px", fontWeight: "600" },
+        ], // Semibold
+        preset2Bold: [
+          "14px",
+          { lineHeight: "150%", letterSpacing: "0px", fontWeight: "700" },
+        ], // Bold
+      },
+      spacing: {
+        500: "40px",
+        300: "24px",
+        200: "16px",
+        150: "12px",
+        100: "8px",
+        50: "4px",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
+
